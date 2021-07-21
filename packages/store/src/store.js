f2ab96f412466da935ccf32fb6265a2287e6bdb1
Mark S. Miller <erights@gmail.com>
2021-07-21 14:45:30 -0700

    fix: zoe passes tests

--- packages/store/src/store.js
+++ packages/store/src/store.js
@@ -6,14 +6,18 @@ import { assert, details as X, q } from '@agoric/assert';
 import { passStyleOf } from '@agoric/marshal';
 import { mustBeComparable } from '../../same-structure';
 
-const assertKey = key => {
-  harden(key); // TODO: Just a transition kludge. Remove when possible.
-  mustBeComparable(key);
+const assertKey = (key, passableOnly) => {
+  if (passableOnly) {
+    harden(key); // TODO: Just a transition kludge. Remove when possible.
+    mustBeComparable(key);
+  }
 };
 
-const assertValue = value => {
-  harden(value); // TODO: Just a transition kludge. Remove when possible.
-  passStyleOf(value); // asserts that value is passable
+const assertValue = (value, passableOnly) => {
+  if (passableOnly) {
+    harden(value); // TODO: Just a transition kludge. Remove when possible.
+    passStyleOf(value); // asserts that value is passable
+  }
 };
 
 /**
@@ -24,10 +28,12 @@ const assertValue = value => {
  * `set` and `delete` are only allowed if the key does already exist.
  *
  * @template K,V
- * @param  {string} [keyName='key'] - the column name for the key
+ * @param {string} [keyName='key'] - the column name for the key
+ * @param {Partial<{passableOnly: boolean=true}>=} opt transitional. Beware
+ * the default passableOnly will switch to true and ultimately be retired.
  * @returns {Store<K,V>}
  */
-export function makeStore(keyName = 'key') {
+export function makeStore(keyName = 'key', { passableOnly = true } = {}) {
   const store = new Map();
   const assertKeyDoesNotExist = key =>
     assert(!store.has(key), X`${q(keyName)} already registered: ${key}`);
@@ -35,28 +41,28 @@ export function makeStore(keyName = 'key') {
     assert(store.has(key), X`${q(keyName)} not found: ${key}`);
   return harden({
     has: key => {
-      assertKey(key);
+      assertKey(key, passableOnly);
       return store.has(key);
     },
     init: (key, value) => {
-      assertKey(key);
-      assertValue(value);
+      assertKey(key, passableOnly);
+      assertValue(value, passableOnly);
       assertKeyDoesNotExist(key);
       store.set(key, value);
     },
     get: key => {
-      assertKey(key);
+      assertKey(key, passableOnly);
       assertKeyExists(key);
       return store.get(key);
     },
     set: (key, value) => {
-      assertKey(key);
-      assertValue(value);
+      assertKey(key, passableOnly);
+      assertValue(value, passableOnly);
       assertKeyExists(key);
       store.set(key, value);
     },
     delete: key => {
-      assertKey(key);
+      assertKey(key, passableOnly);
       assertKeyExists(key);
       store.delete(key);
     },
--- packages/store/src/weak-store.js
+++ packages/store/src/weak-store.js
@@ -7,23 +7,29 @@ import { passStyleOf } from '@agoric/marshal';
 import { mustBeComparable } from '../../same-structure';
 import './types.js';
 
-const assertKey = key => {
-  harden(key); // TODO: Just a transition kludge. Remove when possible.
-  mustBeComparable(key);
+const assertKey = (key, passableOnly) => {
+  if (passableOnly) {
+    harden(key); // TODO: Just a transition kludge. Remove when possible.
+    mustBeComparable(key);
+  }
 };
 
-const assertValue = value => {
-  harden(value); // TODO: Just a transition kludge. Remove when possible.
-  passStyleOf(value); // asserts that value is passable
+const assertValue = (value, passableOnly) => {
+  if (passableOnly) {
+    harden(value); // TODO: Just a transition kludge. Remove when possible.
+    passStyleOf(value); // asserts that value is passable
+  }
 };
 
 /**
  * @template {Record<any, any>} K
  * @template {any} V
  * @param {string} [keyName='key']
+ * @param {Partial<{passableOnly: boolean=true}>=} opt transitional. Beware
+ * the default passableOnly will switch to true and ultimately be retired.
  * @returns {WeakStore<K, V>}
  */
-export function makeWeakStore(keyName = 'key') {
+export function makeWeakStore(keyName = 'key', { passableOnly = true } = {}) {
   const wm = new WeakMap();
   const assertKeyDoesNotExist = key =>
     assert(!wm.has(key), X`${q(keyName)} already registered: ${key}`);
@@ -31,28 +37,28 @@ export function makeWeakStore(keyName = 'key') {
     assert(wm.has(key), X`${q(keyName)} not found: ${key}`);
   return harden({
     has: key => {
-      assertKey(key);
+      assertKey(key, passableOnly);
       return wm.has(key);
     },
     init: (key, value) => {
-      assertKey(key);
-      assertValue(value);
+      assertKey(key, passableOnly);
+      assertValue(value, passableOnly);
       assertKeyDoesNotExist(key);
       wm.set(key, value);
     },
     get: key => {
-      assertKey(key);
+      assertKey(key, passableOnly);
       assertKeyExists(key);
       return wm.get(key);
     },
     set: (key, value) => {
-      assertKey(key);
-      assertValue(value);
+      assertKey(key, passableOnly);
+      assertValue(value, passableOnly);
       assertKeyExists(key);
       wm.set(key, value);
     },
     delete: key => {
-      assertKey(key);
+      assertKey(key, passableOnly);
       assertKeyExists(key);
       wm.delete(key);
     },
